d57abb540722311094bcf6f73b2b8d8e2f559753
flyway666 <zj0078@gmail.com>
2026-09-13 12:28:58 +0800

    新增切换账号时候停顿，从而使得切换账号不卡顿

--- app/src/main/java/io/github/aw1y2z/sesame/hook/ApplicationHook.java
+++ app/src/main/java/io/github/aw1y2z/sesame/hook/ApplicationHook.java
@@ -247,6 +247,14 @@ public class ApplicationHook extends XposedModule {
                                 initHandler(true);
                                 Log.record("用户已切换");
                                 Toast.show("用户已切换");
+								new Thread(() -> {
+                                    try {
+                                        Thread.sleep(1000);
+                                        initHandler(true);
+                                    } catch (Throwable th) {
+                                        Log.printStackTrace(TAG, th);
+                                    }
+                                }, "Sesame-SwitchUser").start();
                                 return;
                             }
                             UserIdMap.initUser(targetUid);
