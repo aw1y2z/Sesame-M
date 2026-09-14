8dc9d10bbf5f1f12fbda4980284f9ebadc77c17f
flyway666 <160370000+flyway666@users.noreply.github.com>
2026-09-14 09:27:59 +0800

    Update UserIdMap.java
    修复错误

--- app/src/main/java/io/github/aw1y2z/sesame/util/idMap/UserIdMap.java
+++ app/src/main/java/io/github/aw1y2z/sesame/util/idMap/UserIdMap.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Field;
 import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 public class UserIdMap {
